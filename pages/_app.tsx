7f9184915bfb1f4b93bd319950da6a4f94d0ae38
Iskren <iskren.dev@gmail.com>
2023-02-05 21:09:19 +0100

    Revert "feat:"
    This reverts commit edf590204b2957e0b7b3d38895db88c030dbe6f3.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,6 @@
-import '../styles/globals.css'
+import '@/componentsstyles/globals.css'
 import type { AppProps } from 'next/app'
-import { ApolloProvider } from '@apollo/client';
-import client from '../src/services/apollo-client';
 
-export default function SpaceXEnergyClient({ Component, pageProps }: AppProps) {
-	return (
-		<ApolloProvider client={client} >
-			<Component {...pageProps} />
-		</ApolloProvider>
-	)
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
