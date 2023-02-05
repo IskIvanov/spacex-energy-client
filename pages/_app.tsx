edf590204b2957e0b7b3d38895db88c030dbe6f3
Iskren <iskren.dev@gmail.com>
2023-02-05 21:08:58 +0100

    feat:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/componentsstyles/globals.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client';
+import client from '../src/services/apollo-client';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function SpaceXEnergyClient({ Component, pageProps }: AppProps) {
+	return (
+		<ApolloProvider client={client} >
+			<Component {...pageProps} />
+		</ApolloProvider>
+	)
 }
